077d4cc91aa84719a87c5e1ef467858d359b5ccb
Angelo Martin <angeloqmartin@gmail.com>
2018-10-20 15:54:52 -0700

    added function to get youtube api data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-const $searchBtn = $('<button id="search-btn"><img src="img/searchIcon.png" alt="a magnifying glass search icon"></button>');
+const $searchBtn = $('<button type="submit" role="search" id="search-btn"><img src="img/searchIcon.png" alt="a magnifying glass search icon"></button>');
+const $searchTerm = $('#search-bar').val();
 
-$('.searchContainer').append($searchBtn);
+$('.searchContainer').append($searchBtn); // places search button on page
+
+// get youtube API data
+function getAPIdata () {
+    $.ajax({
+        method: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            part: 'snippet',
+            key: 'AIzaSyCkbdTrRPuT_qdEr0tkgLq4Z6Trlvxp4UA',
+            q: $searchTerm,
+            success: function(){
+                console.log('fetehed API data');
+            } // end success function
+
+        } // end data
+    }); // end ajax
+}; // end getAPIdata
